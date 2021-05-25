a63396ae20cbb63567b6c0475f0059cbe994eeee
s542932 <84051396+s542932@users.noreply.github.com>
2021-05-25 15:09:07 -0500

    Update built-in-min-max.py

--- built-in-min-max.py
+++ built-in-min-max.py
@@ -9,7 +9,7 @@ return max_value
 print('The maximum value using the code provided to find the MAXIMUM of any 3 values =',maximum(12,27,36),'\n')
 print('The maximum value calling the build-in max() fuction =', max(12,27,36),'\n')
 
-if maximum(12,27,36) = max(12,27,36):
+if maximum(12,27,36) == max(12,27,36):
   print('YES!  MAXIMUM does = Max \n')
    
 print('The minimum value calling the built-in min() fuction =',min(15,9,27,14),'\n')
@@ -28,5 +28,5 @@ return min_value
 print('The minimum value using the code provided to find the MINIMUM of any 4 values =',minimum(15,9,27,14),'\n')
 print('The minimum value calling the build-in min() fuction =', min(15,9,27,14),'\n')
 
-if minimum(15,9,27,14) = min(15,9,27,14):
+if minimum(15,9,27,14) == min(15,9,27,14):
   print('YES!  MINIMUM does = Min \n')
