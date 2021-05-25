7f6b4f5d5bb158d680f0d3a8f4d21d43a090cfc4
s542932 <84051396+s542932@users.noreply.github.com>
2021-05-25 15:12:17 -0500

    Update built-in-min-max.py

--- built-in-min-max.py
+++ built-in-min-max.py
@@ -1,3 +1,5 @@
+#Maximum
+
 def maximum(value1, value2, value3):
   max_value = value1
   if value2 > max_value:
@@ -6,12 +8,14 @@ def maximum(value1, value2, value3):
     max_value = value3
   return max_value
 
-print('The maximum value using the code provided to find the MAXIMUM of any 3 values =',maximum(12,27,36),'\n')
+print('\n\nThe maximum value using the code provided to find the MAXIMUM of any 3 values =',maximum(12,27,36),'\n')
 print('The maximum value calling the build-in max() fuction =', max(12,27,36),'\n')
 
 if maximum(12,27,36) == max(12,27,36):
   print('YES!  MAXIMUM does = Max \n')
-   
+
+#Minimum  
+  
 print('The minimum value calling the built-in min() fuction =',min(15,9,27,14),'\n')
 
 def minimum(value1, value2, value3, value4):
@@ -24,9 +28,7 @@ def minimum(value1, value2, value3, value4):
     min_value = value4
   return min_value
 
-
 print('The minimum value using the code provided to find the MINIMUM of any 4 values =',minimum(15,9,27,14),'\n')
-print('The minimum value calling the build-in min() fuction =', min(15,9,27,14),'\n')
 
 if minimum(15,9,27,14) == min(15,9,27,14):
-  print('YES!  MINIMUM does = Min \n')
+  print('YES!  MINIMUM does = Min \n\n')
