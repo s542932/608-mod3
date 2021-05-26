6d60a020bf0fd99dab321780b79edd4c1a597b51
s542932 <84051396+s542932@users.noreply.github.com>
2021-05-26 05:55:19 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,4 @@
+import math
 
+print('\nThe square root of 900 using the call math function =',math.sqrt(900))
+print('\nThe rounded value of 9.2 using the call math function =',math.floor(9.2),'\n')
