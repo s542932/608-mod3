233bbf36aa6ec9a897a99ea86808cf5888da67f4
s542932 <84051396+s542932@users.noreply.github.com>
2021-05-29 14:10:34 -0500

    Update population-dispersion.py

--- population-dispersion.py
+++ population-dispersion.py
@@ -5,7 +5,7 @@ print('\nThe population standar deviation for the same dataset (using statistic.
 print('\nThe population standar deviation for the same dataset (using math.sqrt & statistic.pvariance to confirm statistic.pstdev) = ',
       math.sqrt(statistics.pvariance([1,3,4,2,6,5,3,4,5,2])),'\n')     
 if statistics.pstdev([1,3,4,2,6,5,3,4,5,2]) == math.sqrt(statistics.pvariance([1,3,4,2,6,5,3,4,5,2])):
-  print('\nConfirmed math.sqrt & statistic.pvariance is = to statistic.pstdev')
+  print('\nConfirmed math.sqrt & statistic.pvariance is = to statistic.pstdev.\n\n')
 else:
-  print('\nNot equal')
+  print('\nNot equal.\n\n')
 
