39734eca2ab707c312baa03c4a7c4d09aa4a67fe
s542932 <84051396+s542932@users.noreply.github.com>
2021-05-29 14:26:20 -0500

    Update bonus.py

--- bonus.py
+++ bonus.py
@@ -1,1 +1,15 @@
+  
+import statistics
+import math
+import random
 
+range(100)
+print(random.randrange(1,101), end=' ')
+print('\nThe population variance for a random dataset of 100 numbers = ',statistics.pvariance(random.randrange(0,101),'\n')
+#print('\nThe population standard deviation for the same dataset (using statistic.pstdev) = ',statistics.pstdev([1,3,4,2,6,5,3,4,5,2]),'\n')
+#print('\nThe population standard deviation for the same dataset (using math.sqrt & statistic.pvariance to confirm statistic.pstdev) = ',
+#      math.sqrt(statistics.pvariance([1,3,4,2,6,5,3,4,5,2])),'\n')     
+#if statistics.pstdev([1,3,4,2,6,5,3,4,5,2]) == math.sqrt(statistics.pvariance([1,3,4,2,6,5,3,4,5,2])):
+#  print('\nConfirmed math.sqrt & statistic.pvariance is = to statistic.pstdev.\n\n')
+#else:
+#  print('\nNot equal.\n\n')
